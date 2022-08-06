b390cbf586eff8440327ebe7e9380af368d4ee34
woshishabii <jgbsxx20130315@163.com>
2022-08-06 15:30:26 +0800

    no fog

--- main.py
+++ main.py
@@ -892,7 +892,7 @@ def setup():
     # as smooth."
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
-    setup_fog()
+    # setup_fog()
 
 
 def main():
